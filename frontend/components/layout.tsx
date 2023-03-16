f77447aeb72666419a97bfb6d13cc8957373bb04
Carl Lippert <carl.lippert@gmail.com>
2023-03-16 22:48:49 +0000

    making it shiny.

--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: any }) => {
   return (
     <div data-theme="cupcake" className="min-h-screen flex flex-col">
       <Head>
-        <title>GumRua</title>
+        <title>Gumrua - web3 gumroad</title>
         <meta content="web3 gumroad" name="description" />
         <link
           href="https://assets-global.website-files.com/6171b265e5c8aa59b42c3472/619bf6d5140958874014528c_favicon.png"
@@ -13,10 +13,8 @@ const Layout = ({ children }: { children: any }) => {
         />
       </Head>
       <Header />
-      <main className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 pb-12 flex-1">
-        {children}
-      </main>
-      {/* <footer className="h-32">Footer</footer> */}
+      {children}
+      <footer className="h-32">Footer</footer>
     </div>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,9 +9,35 @@ import Layout from "../components/layout";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Image src={logo} width="1200" height="400" alt="gumrua logo" />
-      <div className="border-y-2  border-solid border-black h-16">
-        {/* <Upload /> */}
+      <div className="w-full md:ml-6 md:pr-6 pl-1 md:-mt-8">
+        <Image
+          src={logo}
+          width="1500"
+          height="300"
+          alt="gumrua logo"
+          // className="bg-pink-200"
+        />
+      </div>
+
+      <div className="border-y-2 border-solid border-black h-20">
+        <div className="flex flex-row h-full justify-center">
+          {/* a button that is black text that has an underline when hovered */}
+          <button className="text-black font-2xl hover:underline hover:underline-offset-4 h-full w-32">
+            README
+          </button>
+          <div className="flex-1 h-full" />
+          <button className="text-white bg-black border-l-2 border-black font-3xl hover:text-black h-full w-40 hover:bg-[#ff90e8]">
+            Start Selling
+          </button>
+        </div>
+      </div>
+      <div className="h-[550px] bg-pink-200 flex flex-row border-solid border-black border-b-2">
+        <div className="flex-1 bg-[#ff90e8] border-r-2 border-black border-solid">
+          Copy
+        </div>
+        <div className=" flex-1 bg-[#ffc900] border-r-2 border-black border-solid">
+          Copy
+        </div>
       </div>
     </Layout>
   );
