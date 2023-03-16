25738c95f27e5b9f571edd5fb859680c461fe0ec
mattiapomelli <mattiapomelli@gmail.com>
2023-03-16 16:46:30 +0000

    Add hook to get product bought by user

---
+++ frontend/hooks/use-bought-products.ts
@@ -0,0 +1,36 @@
+import { useAccount, useQuery } from "wagmi";
+import { Product } from "../types/products";
+import { useGumrua } from "./use-gumrua";
+
+export const useBoughtProducts = () => {
+  const { address } = useAccount();
+  const gumrua = useGumrua();
+
+  return useQuery<Product[] | undefined>(
+    ["bought-products", address],
+    async () => {
+      if (!gumrua || !address) return;
+
+      /* Get requests */
+      const products: Product[] = [];
+      const eventFilter = gumrua.filters.ProductBought(null, address);
+      const events = await gumrua.queryFilter(eventFilter);
+
+      for (const event of events) {
+        if (!event.args) continue;
+
+        const id = event.args._productId;
+        const product = await gumrua.products(id);
+
+        products.push({
+          id: id.toNumber(),
+          name: product.name,
+          price: product.price,
+          seller: product.seller as `0x${string}`,
+        });
+      }
+
+      return products;
+    }
+  );
+};
