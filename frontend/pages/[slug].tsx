be636513c7e265366599adbb96f7c94521172f3a
mattiapomelli <mattiapomelli@gmail.com>
2023-03-16 20:44:02 +0000

    Setup product page

--- frontend/pages/[slug].tsx
+++ frontend/pages/[slug].tsx
@@ -1,5 +1,8 @@
 import { ethers } from "ethers";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import { useToken } from "wagmi";
+import { Button } from "../components/basic/button";
 import Layout from "../components/layout";
 import { Spinner } from "../components/Spinner";
 import { useBuyProduct } from "../hooks/use-buy-product";
@@ -25,16 +28,30 @@ const PurchaseInner = ({ slug }: { slug: string }) => {
 
   return (
     <Layout>
-      <h1 className="text-4xl font-bold">{product.name}</h1>
-      <p className="mt-4 text-lg">
-        Price: {ethers.utils.formatEther(product.price)}
-      </p>
-      <button
-        className="bg-blue-500 text-lg py-1 px-4 rounded-md mt-4"
-        onClick={onBuyProduct}
-      >
-        Buy
-      </button>
+      <div className="max-w-[30rem] mx-auto mt-10">
+        <div className="relative h-40 rounded-box overflow-hidden">
+          <Image
+            src={product.image}
+            layout="fill"
+            objectFit="cover"
+            alt="Product"
+          />
+        </div>
+        <h1 className="text-5xl font-medium mt-4">{product.name}</h1>
+        <p className="mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          velit rerum reprehenderit natus omnis eligendi iure amet fugit
+          assumenda cumque id ad qui quos alias odit iusto provident. Nostrum
+          accusamus quae iure quod maiores!
+        </p>
+        <div className="bg-base-200 rounded-box mt-5 mb-2 flex justify-between items-center px-4 py-3">
+          <b>Price: </b>
+          <span>{ethers.utils.formatEther(product.price)} xDAI</span>
+        </div>
+        <Button className="mt-2" block onClick={onBuyProduct}>
+          Buy
+        </Button>
+      </div>
     </Layout>
   );
 };
