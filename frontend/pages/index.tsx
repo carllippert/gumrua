d3984146bdaa29f20c309dcad598dadd4359f3f8
Carl Lippert <carl.lippert@gmail.com>
2023-03-17 17:53:29 +0000

    og girl

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import logo from "../public/gumrua.svg";
+import og_girl from "../public/og_girl.png";
 import Image from "next/image";
 import Layout from "../components/layout";
 import Link from "next/link";
@@ -32,7 +33,7 @@ const Home: NextPage = () => {
       </div>
       {/* Hero */}
       <div className="flex lg:h-[550px] h-[1100px] m-0 w-full bg-blue-200 flex-col p-0 gap-0 lg:flex-row border-solid border-black border-b-2">
-        <div className="flex-1 w-full h-full bg-[#ff90e8] border-b-2 lg:border-b-0 border-black border-solid ">
+        <div className="flex-1 w-full h-full bg-[#ff90e8] border-b-2 lg:border-b-0 border-black border-solid lg:border-r-2">
           <div className="mx-auto my-auto max-w-[40rem] mt-16 lg:px-12 px-4">
             <div className="md:text-8xl text-6xl font-semibold py-4">
               Go from 0 to €1
@@ -43,14 +44,22 @@ const Home: NextPage = () => {
             </div>
             <Button
               size="lg"
-              className="hover:bg-[#ff90e8] border-2 mt-6 hover:text-black font-inherit"
+              className="hover:bg-[#ff90e8] border-2 mt-6 hover:text-black font-inherit  w-full"
             >
               Start Selling
             </Button>
           </div>
         </div>
-        <div className="flex-1 w-full h-full bg-[#ffc900] lg:border-r-2 border-black border-solid">
-          Copy
+        <div className="flex-1 w-full h-full bg-[#ffc900]  border-black border-solid">
+          <div className="lg:ml-20 lg:mt-6 mt-10">
+            <Image
+              src={og_girl}
+              width="500"
+              height="500"
+              alt="gumrua logo"
+              className=""
+            ></Image>
+          </div>
         </div>
       </div>
     </Layout>
