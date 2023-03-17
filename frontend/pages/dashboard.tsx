8bc4af9b1b705ef25fa524da213bdcca598955e4
mattiapomelli <mattiapomelli@gmail.com>
2023-03-17 23:36:37 +0000

    Add link to account and sell page

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -23,13 +23,20 @@ const DashboardPage = () => {
     <Layout>
       <Navbar currentPage="dashboard" />
       <Container className="mt-10 max-w-[50rem]">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col sm:flex-row justify-between sm:items-center gap-2">
           <h1 className="text-4xl font-bold">Dashboard</h1>
-          <Link href="/buy-crypto">
-            <a>
-              <Button>Buy crypto</Button>
-            </a>
-          </Link>
+          <div className="flex gap-2">
+            <Link href="/account">
+              <a>
+                <Button variant="outline">Account</Button>
+              </a>
+            </Link>
+            <Link href="/create">
+              <a>
+                <Button>Sell</Button>
+              </a>
+            </Link>
+          </div>
         </div>
         <Tabs items={items} className="mt-12 mb-8" />
       </Container>
