f0e4d0163f09a9e88bf3bb453f514c6c9339d35a
mattiapomelli <mattiapomelli@gmail.com>
2023-03-17 16:45:17 +0000

    Show loading state for ibn

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../components/basic/button";
+import { Spinner } from "../components/basic/spinner";
 import { Tabs } from "../components/basic/tabs";
 import Container from "../components/container";
 import { CreatedProducts } from "../components/created-products";
@@ -6,8 +7,42 @@ import Layout from "../components/layout";
 import { PurchasedProducts } from "../components/purchased-products";
 import { useOnRamp } from "../context/on-ramp-provider";
 
+const IbanBanner = () => {
+  const { connect, iban, loading } = useOnRamp();
+
+  if (loading) {
+    return (
+      <div className="bg-secondary/20 px-4 py-4 flex justify-center gap-4 items-center min-h-[80px]">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-secondary/20 px-4 py-4 flex justify-between gap-4 items-center">
+      {iban ? (
+        <div>
+          <p>
+            Your IBAN: <span className="font-bold">{iban}</span>
+          </p>
+          <p>You can obtain crypto by sending a bank transfer to this IBAN</p>
+        </div>
+      ) : (
+        <>
+          <p>
+            Create an IBAN to simplify your experience on Gumrua. As a buyer,
+            you can obtain crypto by simply sending a bank transfer to this
+            IBAN.
+          </p>
+          <Button onClick={connect}>Create IBAN</Button>
+        </>
+      )}
+    </div>
+  );
+};
+
 const DashboardPage = () => {
-  const { connect, iban } = useOnRamp();
+  const { connect, iban, loading } = useOnRamp();
 
   const items = [
     {
@@ -25,27 +60,7 @@ const DashboardPage = () => {
       <Container className="mt-10 max-w-[50rem]">
         <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
 
-        <div className="bg-secondary/20 px-4 py-4 flex justify-between gap-4 items-center">
-          {iban ? (
-            <div>
-              <p>
-                Your IBAN: <span className="font-bold">{iban}</span>
-              </p>
-              <p>
-                You can obtain crypto by sending a bank transfer to this IBAN
-              </p>
-            </div>
-          ) : (
-            <>
-              <p>
-                Create an IBAN to simplify your experience on Gumrua. As a
-                buyer, you can obtain crypto by simply sending a bank transfer
-                to this IBAN.
-              </p>
-              <Button onClick={connect}>Create IBAN</Button>
-            </>
-          )}
-        </div>
+        <IbanBanner />
 
         <Tabs items={items} className="mt-8 mb-8" />
       </Container>
